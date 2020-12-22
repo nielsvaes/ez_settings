522726ccdb144b952039c39faa5c42f9c5a6750a
nisse <nielsvaes@gmail.com>
2020-12-22 17:19:21 +0100

    Adding setup.py

---
+++ setup.py
@@ -0,0 +1,21 @@
+import sys
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="ez_settings",
+    version="1.0.0",
+    author="Niels Vaes",
+    author_email="niels.vaes@gmail.com",
+    description="Easy settings for python applications",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/nielsvaes/ez_settings",
+    install_requires=['json'],
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Operating System :: OS Independent",
+    ]
+)
